0e93842aedfff2ae616f8a4b3d9488378eaa5c20
ZKiskinova <zlatina.kiskinova@seznam.cz>
2023-04-01 13:36:44 +0200

    @FindBy(css="") 	private WebElement webElement;

--- src/main/java/fouth/FourthHomeWork.java
+++ src/main/java/fouth/FourthHomeWork.java
@@ -6,13 +6,16 @@ public class FourthHomeWork {
         int[] defaultValues = new int[20];
         int i = 0;
 
-
+        /// Task 1
         while (i < defaultValues.length) {
-            defaultValues[i] = i*5;
+            defaultValues[i] = i * 5;
             System.out.println("The product of index " + i + " and integer 5 is: " + i * 5);
             i++;
 
         }
+        System.out.println(".............................");
+
+        ///// Task 2
         float sum = 0;
         i = 0;
 
@@ -26,8 +29,24 @@ public class FourthHomeWork {
         avarageValue = (sum / defaultValues.length);
 
         System.out.printf("Avarage value of all indexes is: %f\n", avarageValue);
+
+        System.out.println("....................................");
+
+        //// Task 3
+
+        float sumEven = 0;
+        float sumOdd = 0;
+
+        while (i < 19) {
+            if (defaultValues[i] % 2 == 0) {
+                sumEven += defaultValues[i];
+                i++;
+            }
+        }
+        System.out.printf(" value is : %f\n", sumEven);
     }
     }
+
 
 
 
