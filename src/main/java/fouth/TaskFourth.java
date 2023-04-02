f047334ce619f80f4b4a5b6b598742e64ab709d9
ZKiskinova <zlatina.kiskinova@seznam.cz>
2023-04-02 15:51:28 +0200

    @FindBy(css="") 	private WebElement webElement;

--- src/main/java/fouth/TaskFourth.java
+++ src/main/java/fouth/TaskFourth.java
@@ -2,11 +2,17 @@ package fouth;
 
 public class TaskFourth {
     public static void main(String[] args) {
-        int n = 27;
+        int n = 0;
 
-        for (int i = 1; i <= n; i++) {
-            System.out.printf("The number is: %d\n", i);
-        }
-
+        printNumbers(27);
     }
-}
+        public static void printNumbers ( int n){
+
+
+            for (int i = 1; i <= n; i++) {
+                System.out.printf("The number is: %d\n", i);
+            }
+
+        }
+    }
+    
