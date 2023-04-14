a449e8e389d111d9fe91a0a5175c8c30f9e8eade
ZKiskinova <zlatina.kiskinova@seznam.cz>
2023-04-14 19:01:30 +0200

    @FindBy(css="") 	private WebElement webElement;

--- src/main/java/eight/randomEight.java
+++ src/main/java/eight/randomEight.java
@@ -1,14 +1,18 @@
 package eight;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class randomEight {
     public static void main(String[] args) {
 
+        ////Task 3
 
-        int [] array = new Random().ints(10,20).toArray();
-        System.out.println(Arrays.toString(array));
+         Random a = new Random();
+         int[] array = new int[10];
 
+        for (int i = 0; i < array.length; i++) {
+            array[i] = a.nextInt(100);
+            System.out.print(array[i] + " ");
+        }
     }
 }
